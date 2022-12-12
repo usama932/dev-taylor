02b45671455c250a36c94b74f6dea87452867f7c
Usama Qayyum <78843627+usama932@users.noreply.github.com>
2022-12-12 22:37:59 +0500

    Update admin.blade.php

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -7,6 +7,7 @@
       <meta name="csrf-token" content="{{ csrf_token() }}">
       <title>{{ trans('panel.site_title') }}</title>
       <link rel="shortcut icon" href="{{ asset('assets/img/favicon.ico')}}" />
+       
       <link
          rel="apple-touch-icon"
          sizes="76x76"
